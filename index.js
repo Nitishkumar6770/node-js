6ee207f77b920fb5c64731b621f0346a4f1b5d1f
Nitishkumar6770 <Nitishcr72002@gmail.com>
2025-09-20 15:12:48 +0530

    index.js added

---
+++ index.js
@@ -0,0 +1,14 @@
+const {MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017';
+// const url = mongodb://localhost:27017/';
+const client = new MongoClient(url);
+
+async function getDataFromDb() {
+    const result = await client.connect();
+    const db = result.db('youtube');
+    const collection = db.collection('videos');
+    const data = await collection.find({}).toArray();
+    console.log(data);
+    await client.close();
+}
+getDataFromDb();
