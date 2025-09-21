cbeab2bd5797e6b44c8a2d7aa931683a4fa6c697
Nitishkumar6770 <Nitishcr72002@gmail.com>
2025-09-21 13:50:04 +0530

    dbconnection file separated

---
+++ dbconnection.js
@@ -0,0 +1,16 @@
+const {MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017/';
+const client = new MongoClient(url);
+
+async function dbconnection() {
+    const result = await client.connect();
+    const db = result.db('youtube');
+    const collection = db.collection('videos');
+    console.log(collection.find().toArray().then((data)=> {
+        console.log(data);
+    }).catch((err)=>{
+        console.log(err);
+    }));
+    return collection;
+}
+dbconnection();
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const {MongoClient} = require('mongodb');
-const url = 'mongodb://localhost:27017';
-// const url = mongodb://localhost:27017/';
-const client = new MongoClient(url);
+// const {MongoClient} = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+// // const url = mongodb://localhost:27017/';
+// const client = new MongoClient(url);
 
-async function getDataFromDb() {
-    const result = await client.connect();
-    const db = result.db('youtube');
-    const collection = db.collection('videos');
-    const data = await collection.find({}).toArray();
-    console.log(data);
-    await client.close();
-}
-getDataFromDb();
+// async function getDataFromDb() {
+//     const result = await client.connect();
+//     const db = result.db('youtube');
+//     const collection = db.collection('videos');
+//     const data = await collection.find({}).toArray();
+//     console.log(data);
+//     await client.close();
+// }
+// getDataFromDb();
+
+// // i got this value from mongodb compass
