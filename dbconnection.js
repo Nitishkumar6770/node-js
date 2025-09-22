622ec5af5ae7d9dbf5d0126b65dd75b9fcf55d77
Nitishkumar6770 <Nitishcr72002@gmail.com>
2025-09-22 12:30:34 +0530

    dbconnection updated

--- dbconnection.js
+++ dbconnection.js
@@ -7,7 +7,8 @@ async function dbconnection() {
     const db = result.db('youtube');
     const collection = db.collection('videos');
     const data = await collection.find().toArray();
-    await client.close();
+    console.log(data);
+    // await client.close();
     // console.log(collection.find().toArray().then((data)=> {
     //     console.log(data);
     // }).catch((err)=>{
@@ -16,3 +17,4 @@ async function dbconnection() {
     return collection;
 }
 dbconnection();
+module.exports = dbconnection;
