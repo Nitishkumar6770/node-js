dc7c56ca01ecca4d52055578a1af6a0abfa11b8a
Nitishkumar6770 <Nitishcr72002@gmail.com>
2025-09-23 13:24:56 +0530

    middleware added to index.js

--- index.js
+++ index.js
@@ -14,3 +14,24 @@
 // getDataFromDb();
 
 // // i got this value from mongodb compass
+
+const express = require("express");
+const app = express();
+const reqFilter = (req, res, next) => {
+    if(!req.query.age) {
+        res.send("Please provide age");
+    } else if(req.query.age < 18) {
+        res.send("You cannot access this page");
+    }
+    else {
+        next();
+    }
+}
+app.use(reqFilter);
+app.get("/", (request, response) => {
+    response.send("<h1>Welcome to Home Page</h1>");
+});
+app.get("/user", (req, res) => {
+    res.send("<h1>Welcome to User Page</h1>");
+})
+app.listen(4000);
